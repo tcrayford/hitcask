55c81d93b2a76f2ba149527007ac09afb33b43c0
Tom Crayford <tcrayford@googlemail.com>
2012-10-21 20:06:53 +0100

    read more than one log file on startup

--- Database/Hitcask.hs
+++ Database/Hitcask.hs
@@ -14,19 +14,46 @@ import Database.Hitcask.Restore
 import Database.Hitcask.Get
 import Database.Hitcask.Put
 import Database.Hitcask.Delete
+import System.FilePath.Glob
+import Data.List(sortBy)
+import Data.List.Split(splitOn)
+import Data.Ord(comparing)
+
+logFilesInDir :: FilePath -> IO [FilePath]
+logFilesInDir dir = do
+  (matched, _) <- globDir [compile "*.hitcask.data"] dir
+  return $! sortBy mostRecent (concat matched)
+
+mostRecent :: FilePath -> FilePath -> Ordering
+mostRecent = comparing getTimestamp
+
+getTimestamp :: FilePath -> Int
+getTimestamp = read . head . splitOn "."
 
 openLogFiles :: FilePath -> IO [LogFile]
-openLogFiles _ = return []
+openLogFiles dir = do
+  filenames <- logFilesInDir dir
+  mapM openLogFile filenames
+
+openLogFile :: FilePath -> IO LogFile
+openLogFile filepath = do
+  h <- openFile filepath ReadWriteMode
+  return $! LogFile h filepath
 
 connect :: FilePath -> IO Hitcask
 connect dir = do
   createDirectoryIfMissing True dir
-  let filepath = dir ++ "/current"
+  let filepath = dir ++ "/current.hitcask.data"
   m <- restoreFromFile filepath
-  h <- openFile filepath ReadWriteMode
   t <- newTVarIO $! m
   logs <- openLogFiles dir
-  return $! Hitcask t (LogFile h filepath) logs
+  h <- getOrCreateCurrent dir logs
+  return $! Hitcask t h logs
+
+getOrCreateCurrent :: FilePath -> [LogFile] -> IO LogFile
+getOrCreateCurrent dir logs = if null logs
+    then openLogFile (dir ++ "/current.hitcask.data")
+    else return $! head logs
 
 close :: Hitcask -> IO ()
 close h = hClose (getHandle h)
